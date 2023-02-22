3577d75e18711a25e4e30a7f2ef1cda5dbb8eea3
subhash1236 <subhashsingh1236@gmail.com>
2023-02-22 15:26:31 +0530

    first commit

--- firatclass.java
+++ firatclass.java
@@ -1,14 +1,17 @@
 public class firatclass {
-    public static void bubbleSort(int arr[]){
-        for(int turn=0;turn<arr.length-1;turn++){
-            for(int j=0;j<arr.length-1-turn;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-
+    public static void SelectionSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minpos =i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minpos]>arr[j]){
+                    minpos =j;
+                    
                 }
             }
+            int temp = arr[minpos];
+            arr[minpos]=arr[i];
+            arr[i]=temp;
+
         }
     }
     public static void printArr(int arr[]){
@@ -19,7 +22,7 @@ public class firatclass {
     }
     public static void main(String[] args) {
         int arr[]={5,4,1,3,2};
-        bubbleSort(arr);
+        SelectionSort(arr);
         printArr(arr);
         
     }
