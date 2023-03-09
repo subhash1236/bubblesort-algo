573bf77dc78ef376cdcad2895b1bc2a947bd8497
subhash1236 <subhashsingh1236@gmail.com>
2023-03-09 10:14:41 +0530

    first

--- firatclass.java
+++ firatclass.java
@@ -1,31 +1,17 @@
+import java.util.Scanner;
+
 //slection Sort Ans//
 
 public class firatclass {
-    public static void SelectionSort(int arr[]){
-        for(int i=0;i<arr.length-1;i++){
-            int minpos =i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[minpos]>arr[j]){
-                    minpos =j;
-                    
-                }
+   
             }
-            int temp = arr[minpos];
-            arr[minpos]=arr[i];
-            arr[i]=temp;
-
         }
-    }
-    public static void printArr(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        //out put
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
         }
         System.out.println();
-    }
-    public static void main(String[] args) {
-        int arr[]={5,4,1,3,2};
-        SelectionSort(arr);
-        printArr(arr);
-        
     }
 }
